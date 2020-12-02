4ac08352209d036ee98de78ebdf948d4b0229a1d
Jeram <jeramtejano@gmail.com>
2020-12-02 08:29:44 +0800

    fixed reload issue

--- server.js
+++ server.js
@@ -25,3 +25,11 @@ const server = app.listen(3000, function() {
   const port = server.address().port;
   console.log("Example app listening at http://%s:%s", host, port);
 });
+
+app.get('/*', function(req, res) {
+  res.sendFile(__dirname + '/dist/index.html', function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,25 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
-  context: path.join(__dirname, "src"),
-  entry: ["./main.js"],
-  output: {
-    path: path.join(__dirname, "www"),
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"]
-      },
-      { test: /\.css$/, use: [ 'css-loader' ] }
-    ]
-  }
+    mode: "development",
+    context: path.join(__dirname, "src"),
+    entry: ["./main.js"],
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "bundle.js",
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: ["babel-loader"]
+        },
+        { test: /\.css$/, use: [ 'css-loader' ] }
+        ]
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
 };
